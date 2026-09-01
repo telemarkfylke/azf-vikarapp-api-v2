da771230555f731858ffc3e387bfaa76d16afdd3
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-09-01 08:56:09 +0200

    fix: Actually get the permittedSchool from the permittedSchools string array and append the permittedSchools (the code in the if statement has never been true and has never been hit....)

--- src/lib/jobs/getPermittedLocations.ts
+++ src/lib/jobs/getPermittedLocations.ts
@@ -1,4 +1,5 @@
 import { logger } from "@vestfoldfylke/loglady";
+import { ObjectId } from "mongodb";
 import { mongoDB } from "../../../config.js";
 import type { PermittedLocation } from "../../types/graph.js";
 import type { SchoolDoc } from "../../types/mongo.js";
@@ -30,10 +31,16 @@ export const getPermittedLocations = async (company: string): Promise<PermittedL
   }
 
   for (const location of school.permittedSchools) {
-    if (location._id && location.name) {
+    const permittedSchool: SchoolDoc | null = await findOneByQuery<SchoolDoc>(mongoDB.SCHOOLS_COLLECTION, { _id: new ObjectId(location) });
+    if (!permittedSchool) {
+      logger.error(`${logPrefix} - Permitted school with Id {PermittedSchoolId} registered on company '{Company}', not found`, location, company);
+      throw new Error(`Permitted school with Id ${location} registered on company '${company}', not found`);
+    }
+
+    if (permittedSchool._id && permittedSchool.name) {
       permittedLocations.push({
-        _id: location._id,
-        name: location.name
+        _id: permittedSchool._id,
+        name: permittedSchool.name
       });
     }
   }
